f04465f3248e553b1b3a5eb97d20da4d774415fa
Thoriq Kusuma <thoriqk2@gmail.com>
2023-05-30 06:41:32 +0700

    Add new features to register Changes to be committed: 	modified:   app/views/register/index.php
    Changes not staged for commit:
    	modified:   app/controllers/Register.php
    	modified:   app/core/App.php
    	deleted:    app/core/Constants.php
    	modified:   app/core/Controller.php
    	modified:   app/init.php
    	modified:   app/views/login/index.php
    	modified:   public/css/main.css

--- app/views/register/index.php
+++ app/views/register/index.php
@@ -2,23 +2,24 @@
   <div class="login-wrapper">
     <h1>Register Page</h1>
 
-    <form>
-      <div class="mb-3">
-        <label for="InputEmail" class="input-email">Email</label>
-        <input type="nim" class="form-control" id="InputEmail">
+    <form action="<?= BASE_URL; ?>/register/registerUser" method="post">
+      <div class="form-group mb-3">
+        <label for="NIM">NIM</label>
+        <input type="text" class="form-control" id="InputNIM" name="nim">
       </div>
-      <div class="mb-3">
-        <label for="InputNIM" class="input-nim">NIM</label>
-        <input type="nim" class="form-control" id="InputNIM">
+      <div class="form-group mb-3">
+        <label for="password">Password</label>
+        <input type="password" class="form-control" id="InputPassword" name="password">
       </div>
-      <div class="mb-3">
-        <label for="InputPassword" class="input-password">Password</label>
-        <input type="password" class="form-control" id="InputPassword">
+      <div class="form-group mb-3">
+        <label for="confirmPassword">Confirm Password</label>
+        <input type="password" class="form-control" id="ConfirmPassword" name="confirmPass">
       </div>
-      <div class="mb-3">
+      <div class="form-group mb-3">
         <p>Have an account? <a href="<?= BASE_URL ?>/login"> Log-In </a></p>
       </div>
       <button type="submit" class="btn btn-primary btn-login">Register</button>
     </form>
+
   </div>
 </div>

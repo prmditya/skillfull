13d1c6e60f38ad3a2f5e3184da96d6c7ac71c2b6
Thoriq Kusuma <thoriqk2@gmail.com>
2023-06-02 19:28:08 +0700

    changing color pallete

--- app/views/register/index.php
+++ app/views/register/index.php
@@ -2,15 +2,16 @@
   <div class="register-wrapper mx-auto">
 
     <!-- Register Image -->
-    <div class="register-img-wrapper">
-      <img src="<?= BASE_URL; ?>/img/login_wrapper_background.png" width="500" alt="" />
+    <div class="register-img-wrapper mb-3">
+      <img src="<?= BASE_URL; ?>/img/logo.png" alt="" />
     </div>
-    <h1 class="mb-4 text-center">Register Page</h1>
+
+    <h1 class="mb-4 text-center" style="font-weight: bold;">Register Page</h1>
 
     <?php Flasher::flash(); ?>
 
     <!-- Form Input -->
-    <form action="<?= BASE_URL; ?>/register/registerUser" method="post">
+    <form action=" <?= BASE_URL; ?>/register/registerUser" method="post">
       <div class="my-3">
         <input type="number" class="form-control" id="InputNIM" name="nim" placeholder="NIM" required>
       </div>

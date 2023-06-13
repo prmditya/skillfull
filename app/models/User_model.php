0d2dd61f2276dabda8538b2d5c31682ad02f36c1
yudhisthereal <63510362+yudhisthereal@users.noreply.github.com>
2023-06-13 19:01:09 +0700

    using uuid

--- app/models/User_model.php
+++ app/models/User_model.php
@@ -17,6 +17,20 @@ class User_model
     return $result;
   }
 
+  function guidv4($data = null) {
+    // Generate 16 bytes (128 bits) of random data or use the data passed into the function.
+    $data = $data ?? random_bytes(16);
+    assert(strlen($data) == 16);
+
+    // Set version to 0100
+    $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
+    // Set bits 6-7 to 10
+    $data[8] = chr(ord($data[8]) & 0x3f | 0x80);
+
+    // Output the 36 character UUID.
+    return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+  }
+
   public function addUser($data)
   {
     $nim = $data['nim'];
@@ -30,8 +44,9 @@ class User_model
     }
 
     $password = password_hash($data['password'], PASSWORD_DEFAULT);
+    $uid = $this->guidv4();
 
-    $statement = "INSERT INTO user VALUES('', '$nim', '', '$nim', '$password', '')";
+    $statement = "INSERT INTO user VALUES('$uid', '$nim', '', '$nim', '$password')";
     $this->db->query($statement);
     $result = mysqli_affected_rows($this->db->dbHandler);
     return $result;
